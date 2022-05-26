ee68cac2fd89c71c7617e4c8a783c02afe29a2ac
Eduard <ladiesman5511@mail.ru>
2022-05-26 14:57:27 +0300

    tail,api

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '33522dcbb97b9966948511fb6c5379e8';
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
